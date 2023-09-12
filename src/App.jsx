56647191e00759fffc1b30beb344c864d6dfe9b4
abir16 <abir@shuttlebd.com>
2023-09-12 17:43:02 +0600

    Inital project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => (
+  <main className='relative'>
+    Nav
+  <section className='xl:padding-1 wide:padding-r padding-b'>
+    Hero
+  </section>
+  <section className='padding'>
+    Popular products
+  </section>
+  <section className='padding'>
+    Super quality
+  </section>
+  <section className='padding'>
+    Services
+  </section>
+  <section className='padding'>
+    Special offer
+  </section>
+  
+  </main>
+)
+
+export default App
